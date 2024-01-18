e8e4761a8b0a891ddd6d37f5d800b2f0046e7f34
Lynne <pxlynne@gmail.com>
2024-01-18 01:07:37 -0500

    changed add marker

--- app.js
+++ app.js
@@ -2,20 +2,6 @@ var map = L.map('map').setView([37.5500, -121.9833], 9); // Centered on Greater 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-<<<<<<< HEAD
-var markers = []; // Array to keep track of markers
-
-var locations = [
-    // Example locations with e-waste subcategories
-    { coordinates: [37.5072, -122.2654], category: 'e-waste', subcategory: ['monitors','computers','cell phone','batteries'], info: '<b>Shoreway Environmental Center</b><br>333 Shoreway Road, San Carlos, CA 94070' },
-    { coordinates: [37.4283, -121.9076], category: 'e-waste', subcategory: ['monitors', 'computers'], info: '<b>Green E-Waste Recycling Center</b><br>1664 Watson Court, Milpitas, CA 95035' },
-    { coordinates: [37.59318372955853, -122.03808654232894], category: 'e-waste', subcategory: ['monitors', 'computers'], info: '<b>Recycle1234.com</b><br>33548 Central Ave, Union City CA 94587 USA' },
-    { coordinates: [37.62821693467349, -122.10852946168214], category: 'e-waste', subcategory: ['monitors', 'computers'], info: '<b>Rica Recycling</b><br>27105 Industrial Blvd, Hayward CA ' },
-
-    // Additional locations can be added here...
-];
-
-=======
 var markers = []; 
 
 var locations = [
@@ -78,36 +64,22 @@ var locations = [
 ];
 
 
-<<<<<<< HEAD
->>>>>>> 93c110b (map location)
-// Function to add markers
-=======
 //function to add markers
->>>>>>> 9105d33 (changed add marker func)
 function addMarkers() {
     markers.forEach(marker => marker.remove());
     markers = [];
 
     var selectedCategories = Array.from(document.querySelectorAll('input[type="checkbox"]:checked:not(.e-waste):not(.vendor)')).map(e => e.id);
     var selectedEWasteSubcategories = Array.from(document.querySelectorAll('.e-waste:checked')).map(e => e.value);
-<<<<<<< HEAD
-=======
     var selectedHouseAppSubcategories = Array.from(document.querySelectorAll('.houseApp:checked')).map(e => e.value);
     var selectedClothingSubcategories = Array.from(document.querySelectorAll('.clothing:checked')).map(e => e.value);
->>>>>>> 93c110b (map location)
     var selectedVendorSubcategories = Array.from(document.querySelectorAll('.vendor:checked')).map(e => e.value);
 
     locations.forEach(function(location) {
         if (selectedCategories.length > 0 && !selectedCategories.includes(location.category)) return;
-<<<<<<< HEAD
-
-        if (location.category === 'e-waste' && selectedEWasteSubcategories.length > 0 && !selectedEWasteSubcategories.some(sub => location.subcategory.includes(sub))) return;
-
-=======
         if (location.category === 'e-waste' && selectedEWasteSubcategories.length > 0 && !selectedEWasteSubcategories.some(sub => location.subcategory.includes(sub))) return;
         if (location.category === 'houseApp' && selectedHouseAppSubcategories.length > 0 && !selectedHouseAppSubcategories.some(sub => location.subcategory.includes(sub))) return;
         if (location.category === 'clothing' && selectedClothingSubcategories.length > 0 && !selectedClothingSubcategories.some(sub => location.subcategory.includes(sub))) return;
->>>>>>> 93c110b (map location)
         if (location.category === 'vendor' && selectedVendorSubcategories.length > 0 && !selectedVendorSubcategories.some(sub => location.subcategory.includes(sub))) return;
 
         var marker = L.marker(location.coordinates).addTo(map);
@@ -125,12 +97,7 @@ document.querySelectorAll('input.e-waste').forEach(function(element) {
     element.addEventListener('change', addMarkers);
 });
 
-<<<<<<< HEAD
-addMarkers(); // Add initial markers
-=======
 addMarkers(); 
->>>>>>> 93c110b (map location)
-
 
 
 // Map search bar
@@ -155,11 +122,7 @@ function searchLocation() {
         });
 }
 
-<<<<<<< HEAD
-['e-waste', 'vendor'].forEach(cat => {
-=======
 ['e-waste', 'houseApp', 'clothing','vendor'].forEach(cat => {
->>>>>>> 93c110b (map location)
     document.getElementById(cat).addEventListener('change', function() {
         document.getElementById(cat + '-subcategories').style.display = this.checked ? 'block' : 'none';
         addMarkers();
@@ -167,14 +130,9 @@ function searchLocation() {
 });
 
 
-<<<<<<< HEAD
-// TrashureCalendar
-
-=======
 
 
 // TrashureCalendar
->>>>>>> 93c110b (map location)
 var currentDate = new Date('2023-08-11');
 
 function renderCalendar() {
@@ -212,8 +170,6 @@ document.addEventListener('DOMContentLoaded', renderCalendar);
 
 
 // TrashureCalendar End
-<<<<<<< HEAD
-=======
 
 
 const hamburger = document.querySelector(".hamburger");
@@ -228,4 +184,3 @@ hamburger.addEventListener("click", () => {
 });
 
 
->>>>>>> 93c110b (map location)
